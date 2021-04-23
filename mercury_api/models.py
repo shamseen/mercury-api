9430a917d7df4f6dfd2826c19a358ca08136b2c8
shamseen <shamseen.c@gmail.com>
2021-04-23 11:58:37 -0400

    models 2: goal & real times, DoB, sex, email (#3)

--- mercury_api/models.py
+++ mercury_api/models.py
@@ -2,24 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Runner(models.Model):
-
-    ### Dropdowns
-    CHOICES_SEX = [
-        ("M", "Male"),
-        ("F", "Female"),
-    ]
     
     ### Fields
-    first_name = models.Charfield(max_length=100)
-    last_name = models.Charfield(max_length=100)
-    city = models.Charfield(max_length=100)
-    state = models.Charfield(max_length=100)
-    country = models.Charfield(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     wheelchair = models.BooleanField(default=False)
-    # TBD: dob, sex, goal_time
-
-    ### Dropdown fields
-    # sex = Models.CharField(max_length=2, choices=CHOICES_SEX)
+    goal_time = models.DurationField()
+    real_time = models.DurationField()
+    dob = models.DateField()
+    sex = models.CharField(max_length=20)
 
 # if only one field, maybe we shouldn't have it? stored in results
 # HOWEVER! if we want to expand to different races around the world...
@@ -31,13 +25,17 @@ class Race(models.Model):
 
 ### Junction table for M:n relationship
 class Result(models.Model):
+    # Race category e.g. age group + sex
+    cohort = models.PositiveIntegerField()
 
-     runner_id = models.ForeignKey(
+    runner_id = models.ForeignKey(
         Runner,                     # table name
-        on_delete=models.CASCADE,   # deep delete
-        related_name='comments')    # 1 post => many comments
-
+        on_delete=models.RESTRICT,  # error out if trying to delete
+        null=False,
+        related_name='+')           # can't be called by Runner operations
+        
     race_year = models.ForeignKey(
         Race,                       # table name
-        on_delete=models.CASCADE,   # deep delete
-        related_name='comments')    # 1 post => many comments
+        on_delete=models.RESTRICT,  # error out if trying to delete
+        null = False,              
+        related_name='+')           # can't be called by Race operations
