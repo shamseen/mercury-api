e84202ba6df34b91a9bd2ba75225aa5fdba277bc
shamseen <shamseen.c@gmail.com>
2021-04-23 21:47:28 -0400

    added a superuser

--- mercury_api/admin.py
+++ mercury_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Runner, Race, Result
 
 # Register your models here.
+admin.site.register(Runner)
+admin.site.register(Race)
+admin.site.register(Result)
