5cb1057e35a9fc88f9c00f4a184d90da1e819368
shamseen <shamseen.c@gmail.com>
2021-04-26 14:35:58 -0400

    RACE GET: runners grouped by race only

--- mercury_api/models.py
+++ mercury_api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# if only one field, maybe we shouldn't have it? stored in results
+# HOWEVER! if we want to expand to different races around the world...
+class Race(models.Model):
+    year = models.PositiveIntegerField()
+    # scalability: name, location (city/state/country)
+
 class Runner(models.Model):
 
     ### Fields
@@ -10,6 +15,12 @@ class Runner(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     real_time = models.DurationField(null=True)
+    cohort_id = models.PositiveIntegerField(null=True)
+    race_id = models.ForeignKey(
+        Race,                       # table name
+        on_delete=models.RESTRICT,  # error out if trying to delete
+        null = False,              
+        related_name='runners')     # for db query
 
     # Not in results table
     # goal_time = models.DurationField() # messing with form POST
@@ -18,23 +29,17 @@ class Runner(models.Model):
     dob = models.DateField()
     sex = models.CharField(max_length=20)
 
-    
-# if only one field, maybe we shouldn't have it? stored in results
-# HOWEVER! if we want to expand to different races around the world...
-class Race(models.Model):
-    year = models.PositiveIntegerField()
-    # scalability: name, location (city/state/country)
 
-### Junction table for M:n relationship
-class Result(models.Model):
-    # Race category e.g. age group + sex, determined in react
-    cohort = models.PositiveIntegerField()
+# ### Junction table for M:n relationship
+# class Result(models.Model):
+#     # Race category e.g. age group + sex, determined in react
+#     cohort = models.PositiveIntegerField()
 
-    # Will show a nested array of runner objs
-    runner = models.ManyToManyField(Runner)
+#     # Will show a nested array of runner objs
+#     runner = models.ManyToManyField(Runner)
         
-    race = models.ForeignKey(
-        Race,                       # table name
-        on_delete=models.RESTRICT,  # error out if trying to delete
-        null = False,              
-        related_name='results')     # for db query
+    # race = models.ForeignKey(
+    #     Race,                       # table name
+    #     on_delete=models.RESTRICT,  # error out if trying to delete
+    #     null = False,              
+    #     related_name='results')     # for db query
